abb0f2102eb30216831a1e931a064d2634d5b05f
Castrucci <maximiliano.castrucci@igt.com>
2020-07-16 06:30:25 -0300

    added a dashboard html added session managment refactored code

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require("express");
-const app = express();
+
 const redis = require('redis')
-const client = redis.createClient()
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
+const client = redis.createClient()
 const saltRounds = 10;
+const app = express();
+const __exposedDir = __dirname + "/public/";
+
 
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+    session({
+      store: new RedisStore({ client: client }),
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 36000000, //10 hours, in milliseconds
+        httpOnly: false,
+        secure: false,
+      },
+      secret: 'eN01gOXUV2HN21OnBhWF',
+    })
+  )
 
 app.get("/", (req, res) => {
-    res.sendFile("index.html");
+    if(req.session.userid)
+        res.sendfile(__exposedDir + "dashboard/home.html")
+    else
+        res.sendFile(__exposedDir + "signin/signin.html");
 })
 
 app.post("/signin", (req, res)=>{
@@ -26,35 +49,47 @@ app.post("/signin", (req, res)=>{
             return;
         }
         if (userID){ //in this case user does exist, we will log in
-            client.hget(`user:${userID}`, 'hash', async (err, hash) =>{
-                let result = await bcrypt.compare(password, hash);
-                console.log(result);
-                if(result){
-                    res.send("loged in!");
-                }else
-                    res.send("wrong password!");
-                return;
-                    
-            })
+            handleLogin(userID, password);
         }else { //user does not exist, we will register
-            client.incr('userid', async (err, userIdFromDb) => { // in our DB structure, we increment the ID, this is a new user
-                if (err){
-                    console.log("something went wrong during the registration processs");
-                    res.send("something failed, we are working on it!");
-                    return;
-                }
-                client.hset('users', email, userIdFromDb) // we create a new username and we match it with the new ID
-                //now we will store the hash of the password
-                const hash = await bcrypt.hash(password, saltRounds);
-                //finally, we will create an entry userID - user email - hash
-                client.hset(`user:${userIdFromDb}`, 'hash', hash, 'username', email);
-                res.send("registration complete!");
-                return;
-            });
-            
+            handleSignup(email, password)
         }
-
     });
+
+    const handleLogin = (idFromDB, password) => {
+        client.hget(`user:${idFromDB}`, 'hash', async (err, hash) =>{
+            let result = await bcrypt.compare(password, hash);
+            if(result){ //everything ine, lets redirect him to dashboard
+                saveSessionAndRedirectToDashboard(idFromDB);
+            }else
+                res.send("wrong password!");
+            return;
+                
+        })
+    }
+
+    const handleSignup = (email, password) => {
+        client.incr('userid', async (err, userIdFromDb) => { // in our DB structure, we increment the ID, this is a new user
+            if (err){
+                console.log("something went wrong during the registration processs");
+                res.send("something failed, we are working on it!");
+                return;
+            }
+            client.hset('users', email, userIdFromDb) // we create a new username and we match it with the new ID
+            //now we will store the hash of the password
+            const hash = await bcrypt.hash(password, saltRounds);
+            //finally, we will create an entry userID - user email - hash
+            client.hset(`user:${userIdFromDb}`, 'hash', hash, 'username', email);
+            saveSessionAndRedirectToDashboard(userIdFromDb);
+            return;
+        });    
+    }
+    const saveSessionAndRedirectToDashboard = (userID) => {
+        req.session.userid = userID;
+        req.session.save();
+        res.status(200).sendFile(__exposedDir + "dashboard/home.html");
+    };
 })
+
+
 
 app.listen(3000, () => console.log("server running"));
